a9ecce506499ffb2693ba6d06915f07e1c5943db
Rafael Risalte <risaltte@gmail.com>
2020-08-14 19:01:21 -0400

    App Proffy finalizado conforme a semana NLW

---
+++ public/scripts/addField.js
@@ -0,0 +1,26 @@
+// Procurar o botão
+document.querySelector("#add-time")
+// Quando clicar no botão
+.addEventListener('click', cloneField)
+
+
+// Executar uma ação
+function cloneField() {
+    // Duplicar os campos. Que campos?
+    const newFieldContainer = document.querySelector(".schedule-item").cloneNode(true)
+
+    // pegar os campos: que campos??
+    const fields = newFieldContainer.querySelectorAll('input')
+    
+    // para cada campo, limpar
+    fields.forEach(function(field) { 
+        // pega o field do momento e limpa ele
+
+        field.value = ""
+
+    })
+
+    //Colocar na página: onde??
+    document.querySelector('#schedule-items').appendChild(newFieldContainer)
+
+}
